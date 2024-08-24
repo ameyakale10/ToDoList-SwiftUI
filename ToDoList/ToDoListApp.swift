c9c4c429999d3372d9bf72fbbbd7ed080303dad8
ameyakale10 <ameya.kale1010@gmail.com>
2024-08-24 19:17:35 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ameya Kale on 2024-08-24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
